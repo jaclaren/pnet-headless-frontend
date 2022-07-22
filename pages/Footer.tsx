318b67df16273d8585dc5026dbf79ce6d06456a9
Jani <jani.claren@gmail.com>
2022-07-22 12:09:47 +0200

    Basic footer blockin

---
+++ pages/Footer.tsx
@@ -0,0 +1,51 @@
+export function Footer() {
+  return (
+    <footer className="footer">
+    <div className="footer__column">
+      <ul className="footer__list">
+        <li className="footer__item">
+          <a rel="nofollow" href="https://www.facebook.com/peliarvostelut" className="footer__link footer__link--icon c-icon c-icon__some c-icon__some__facebook">
+                  <svg enable-background="new 0 0 48 48" id="Layer_1" version="1.1" viewBox="0 0 48 48" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        <circle cx="24" cy="24" r="24"></circle>
+          <path d="M29.9,19.5h-4v-2.6c0-1,0.7-1.2,1.1-1.2c0.5,0,2.8,0,2.8,0v-4.4l-3.9,0c-4.4,0-5.3,3.3-5.3,5.3v2.9h-2.5V24  h2.5c0,5.8,0,12.7,0,12.7h5.3c0,0,0-7,0-12.7h3.6L29.9,19.5z"></path>
+      </svg>
+              </a>
+        </li>
+        <li className="footer__item">
+          <a rel="nofollow" href="https://twitter.com/_peliarvostelut" className="footer__link footer__link--icon c-icon c-icon__some c-icon__some__twitter">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 400 400" xmlSpace="preserve">
+      <g id="Dark_Blue">
+      	<circle className="st0" cx="200" cy="200" r="200"></circle>
+      </g>
+      <g id="Logo__x2014__FIXED">
+      	<path className="st1" d="M163.4,305.5c88.7,0,137.2-73.5,137.2-137.2c0-2.1,0-4.2-0.1-6.2c9.4-6.8,17.6-15.3,24.1-25
+      		c-8.6,3.8-17.9,6.4-27.7,7.6c10-6,17.6-15.4,21.2-26.7c-9.3,5.5-19.6,9.5-30.6,11.7c-8.8-9.4-21.3-15.2-35.2-15.2
+      		c-26.6,0-48.2,21.6-48.2,48.2c0,3.8,0.4,7.5,1.3,11c-40.1-2-75.6-21.2-99.4-50.4c-4.1,7.1-6.5,15.4-6.5,24.2
+      		c0,16.7,8.5,31.5,21.5,40.1c-7.9-0.2-15.3-2.4-21.8-6c0,0.2,0,0.4,0,0.6c0,23.4,16.6,42.8,38.7,47.3c-4,1.1-8.3,1.7-12.7,1.7
+      		c-3.1,0-6.1-0.3-9.1-0.9c6.1,19.2,23.9,33.1,45,33.5c-16.5,12.9-37.3,20.6-59.9,20.6c-3.9,0-7.7-0.2-11.5-0.7
+      		C110.8,297.5,136.2,305.5,163.4,305.5"></path>
+      </g>
+      </svg>
+              </a>
+        </li>
+        <li className="footer__item">
+          <a rel="nofollow" href="/yhteydenotto" className="footer__link footer__link--icon c-icon c-icon--circle c-icon__some c-icon__some__contact">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"></path>
+    </svg>
+              </a>
+        </li>
+      </ul>
+    </div>
+    <div className="footer__column footer__column__partners">
+      <h3 className="footer__columnheader">Yhteistyössä</h3>
+      <a href="https://www.gamereality.org" rel="nofollow" className="footer__link footer__link__partner">
+        www.gamereality.org
+      </a>
+    </div>
+    <div className="footer__column footer__copyright">
+      © Peliarvostelut.NET (2018-2022)
+    </div>
+	</footer>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "../styles/globals.css";
 import "../shared/styles/main.scss";
 import type { AppProps } from "next/app";
 import { Navigation } from "../components/Navigation/Navigation";
+import { Footer } from "./Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className="pnet">
-      <Navigation /> <Component {...pageProps} />
+      <Navigation />
+      <Component {...pageProps} />
+      <Footer></Footer>
     </main>
   );
 }
